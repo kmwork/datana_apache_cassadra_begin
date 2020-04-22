23101039a5a88a99bc136433cbc15259505828ef
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-22 19:08:05 +0300

    рабочие документы для кассандры

--- etc/my-test-column-one.sh
+++ etc/my-test-column-one.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-./tools/bin/cassandra-stress user profile=/home/lin/work-lanit/my_datana_cassandra/kostya-one-column-table-cassandra-stress.yaml n=1 "ops(insert=1,simple1=0)"
+#!/bin/sh -v
+./tools/bin/cassandra-stress user profile="~/work-lanit/my_datana_cassandra/kostya-one-column-table-cassandra-stress.yaml" n=1 "ops(insert=1,simple1=0)"
