2a57ca1eb7000f604064e2eedbf1ac0dffbf2b19
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-23 00:54:32 +0300

    рабочие документы для кассандры

--- etc/my-test-column-one.sh
+++ etc/my-test-column-one.sh
@@ -2,4 +2,4 @@
 clear
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
-./tools/bin/cassandra-stress user profile="file:///home/lin/work-lanit/my_datana_cassandra/etc/kostya-one-column-table-cassandra-stress.yaml" n=1 "ops(insert=10,simple1=10)" truncate=always
+./tools/bin/cassandra-stress user profile="file:///home/lin/work-lanit/my_datana_cassandra/etc/kostya-one-column-table-cassandra-stress.yaml" n=500000 "ops(insert=500000,simple1=500000)" truncate=always
