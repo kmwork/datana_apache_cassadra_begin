a9fc3e85f3b050d11d27240d94137e5f1ccf73be
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-22 19:57:03 +0300

    рабочие документы для кассандры

---
+++ etc/my-test-example-2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -v
+clear
+export JAVA_HOME=/home/lin/apps/jdk13
+export PATH=$JAVA_HOME/bin:$PATH
+./tools/bin/cassandra-stress user profile="file:///home/lin/work-lanit/my_datana_cassandra/etc/example-stress-2.yaml" n=1 "ops(insert=1,simple1=0)"
