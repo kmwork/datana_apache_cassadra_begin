0bc7056430bcb525fda296382a56f4f1896f0181
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-04-23 09:30:07 +0300

    тесты вторые с огромной таблицей

--- etc/my-test-column-big.sh
+++ etc/my-test-column-big.sh
@@ -2,4 +2,4 @@
 clear
 export JAVA_HOME=/home/lin/apps/jdk13
 export PATH=$JAVA_HOME/bin:$PATH
-./tools/bin/cassandra-stress user profile="file:///home/lin/work-lanit/my_datana_cassandra/etc/kostya-big-table-cassandra-stress.yaml" n=10 "ops(insert=1,simple_big1=1)"
+./tools/bin/cassandra-stress user profile="file:///home/lin/work-lanit/my_datana_cassandra/etc/kostya-big-table-cassandra-stress.yaml" n=10 "ops(insert=10,simple_big1=1)"
